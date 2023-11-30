149964e833ec35773b876d6136e403b9eb1085fa
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2023-11-30 14:59:06 -0300

    alteração script dbml e correção do modelo ER

--- projeto/Scripts/script-bd-seguro.sql
+++ projeto/Scripts/script-bd-seguro.sql
@@ -1,74 +1,69 @@
--- Tabela Cliente
-CREATE TABLE Cliente (
-    ClienteID INT PRIMARY KEY,
-    Nome VARCHAR(255),
-    Endereco VARCHAR(255),
-    Telefone VARCHAR(15),
-    Email VARCHAR(255)
+CREATE TABLE cliente (
+    cliente_id INT PRIMARY KEY,
+    nome_cliente VARCHAR(255),
+    endereco_cliente VARCHAR(255),
+    telefone_cliente VARCHAR(15),
+    email_cliente VARCHAR(255)
 );
 
--- Tabela Imovel
-CREATE TABLE Imovel (
-    ImovelID INT PRIMARY KEY,
-    Endereco VARCHAR(255),
-    TipoImovel VARCHAR(50),
-    ValorImovel DECIMAL(10, 2)
+CREATE TABLE imovel (
+    imovel_id INT PRIMARY KEY,
+    cliente_id INT REFERENCES cliente(cliente_id),
+    endereco_imovel VARCHAR(255),
+    tipo_imovel VARCHAR(50),
+    valor_imovel DECIMAL(12, 2)
 );
 
--- Tabela ApoliceSeguro
-CREATE TABLE ApoliceSeguro (
-    ApoliceID INT PRIMARY KEY,
-    ClienteID INT,
-    ImovelID INT,
-    DataInicio DATE,
-    DataTermino DATE,
-    Premio DECIMAL(10, 2),
-    FOREIGN KEY (ClienteID) REFERENCES Cliente(ClienteID),
-    FOREIGN KEY (ImovelID) REFERENCES Imovel(ImovelID)
+
+CREATE TABLE apolice_seguro (
+    apolice_id INT PRIMARY KEY,
+    cliente_id INT UNIQUE REFERENCES cliente(cliente_id),
+    imovel_id INT UNIQUE REFERENCES imovel(imovel_id),
+    seguradora_id INT REFERENCES seguradora(seguradora_id),
+    data_inicio DATE,
+    data_termino DATE,
+    premio DECIMAL(12, 2)
 );
 
--- Tabela CoberturaSeguro
-CREATE TABLE CoberturaSeguro (
-    CoberturaID INT PRIMARY KEY,
-    Descricao VARCHAR(255),
-    ValorCobertura DECIMAL(10, 2),
-    ApoliceID INT,
-    FOREIGN KEY (ApoliceID) REFERENCES ApoliceSeguro(ApoliceID)
+
+CREATE TABLE cobertura_seguro (
+    cobertura_id INT PRIMARY KEY,
+    descricao VARCHAR(255),
+    corretor_id INT REFERENCES corretor(corretor_id),
+    valor_cobertura DECIMAL(10, 2),
+    apolice_id INT REFERENCES apolice_seguro(apolice_id)
 );
 
--- Tabela Pagamento
-CREATE TABLE Pagamento (
-    PagamentoID INT PRIMARY KEY,
-    ApoliceID INT,
-    ClienteID INT,
-    DataPagamento DATE,
-    ValorPagamento DECIMAL(10, 2),
-    FOREIGN KEY (ApoliceID) REFERENCES ApoliceSeguro(ApoliceID),
-    FOREIGN KEY (ClienteID) REFERENCES Cliente(ClienteID)
+
+CREATE TABLE pagamento (
+    pagamento_id INT PRIMARY KEY,
+    apolice_id INT REFERENCES apolice_seguro(apolice_id),
+    data_pagamento DATE,
+    valor_pagamento DECIMAL(10, 2)
 );
 
--- Tabela Corretor
-CREATE TABLE Corretor (
-    CorretorID INT PRIMARY KEY,
-    Nome VARCHAR(255),
-    NumeroRegistro VARCHAR(50),
-    Contato VARCHAR(15)
+
+CREATE TABLE corretor (
+    corretor_id INT PRIMARY KEY,
+    nome_corretor VARCHAR(255),
+    numero_registro VARCHAR(50),
+    seguradora_id INT REFERENCES seguradora(seguradora_id),
+    telefone_corretor VARCHAR(15),
+    email_corretor VARCHAR(255)
 );
 
--- Tabela Seguradora
-CREATE TABLE Seguradora (
-    SeguradoraID INT PRIMARY KEY,
-    Nome VARCHAR(255),
-    Endereco VARCHAR(255),
-    Contato VARCHAR(15)
+CREATE TABLE seguradora (
+    seguradora_id INT PRIMARY KEY,
+    nome_seguradora VARCHAR(255) UNIQUE,
+    endereco_seguradora VARCHAR(255),
+    telefone_seguradora VARCHAR(15),
+    email_seguradora VARCHAR(255)
 );
 
--- Tabela EventoSinistro
-CREATE TABLE EventoSinistro (
-    SinistroID INT PRIMARY KEY,
-    ApoliceID INT,
-    DataSinistro DATE,
-    DescricaoSinistro VARCHAR(255),
-    ValorSinistro DECIMAL(10, 2),
-    FOREIGN KEY (ApoliceID) REFERENCES ApoliceSeguro(ApoliceID)
+CREATE TABLE evento_sinistro (
+    sinistro_id INT PRIMARY KEY,
+    apolice_id INT REFERENCES apolice_seguro(apolice_id),
+    data_sinistro DATE,
+    descricao_sinistro VARCHAR(255),
+    valor_sinistro DECIMAL(12, 2)
 );
