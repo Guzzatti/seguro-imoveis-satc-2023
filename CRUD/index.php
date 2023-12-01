3952157c9f21c814fafc12cfcdc63cab2a575f95
Gabriel Guzzatti <136494543+Guzzatti@users.noreply.github.com>
2023-12-01 16:40:55 -0300

    crud
    alteração no crud

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,22 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insurance System</title>
+    <title>Seguro de Imóveis</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <h1>Insurance System</h1>
-
-    <h2>Cliente</h2>
-    <form action="processar_cliente.php" method="post">
-        Nome: <input type="text" name="nome_cliente"><br>
-        Endereco: <input type="text" name="endereco_cliente"><br>
-        Telefone: <input type="text" name="telefone_cliente"><br>
-        Email: <input type="text" name="email_cliente"><br>
-        <input type="submit" value="Adicionar Cliente">
-    </form>
-
     <?php
     // Conectar ao banco de dados (substitua com suas credenciais)
     $servername = "localhost";
@@ -31,34 +19,70 @@
         die("Erro na conexão: " . $conn->connect_error);
     }
 
-    // Consultar os clientes no banco de dados
-    $sql = "SELECT * FROM Cliente";
-    $result = $conn->query($sql);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Verificar se um novo cliente está sendo adicionado
+        if (isset($_POST['adicionar_cliente'])) {
+            // Obter os dados do formulário
+            $nomeCliente = $_POST['nome_cliente'];
+            $enderecoCliente = $_POST['endereco_cliente'];
+            $telefoneCliente = $_POST['telefone_cliente'];
+            $emailCliente = $_POST['email_cliente'];
 
-    if ($result->num_rows > 0) {
-        // Exibir os clientes em uma tabela
-        echo "<h2>Lista de Clientes</h2>";
-        echo "<table>";
-        echo "<tr><th>ID</th><th>Nome</th><th>Endereço</th><th>Telefone</th><th>Email</th></tr>";
+            // Preparar a consulta SQL para inserir os dados
+            $sql = "INSERT INTO cliente (nome_cliente, endereco_cliente, telefone_cliente, email_cliente) VALUES ('$nomeCliente', '$enderecoCliente', '$telefoneCliente', '$emailCliente')";
 
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>".$row['ClienteID']."</td>";
-            echo "<td>".$row['Nome']."</td>";
-            echo "<td>".$row['Endereco']."</td>";
-            echo "<td>".$row['Telefone']."</td>";
-            echo "<td>".$row['Email']."</td>";
-            echo "</tr>";
+            if ($conn->query($sql) === TRUE) {
+                echo "Cliente adicionado com sucesso.";
+            } else {
+                echo "Erro ao adicionar o cliente: " . $conn->error;
+            }
         }
 
-        echo "</table>";
-    } else {
-        echo "Nenhum cliente encontrado.";
+        // Verificar se um cliente existente está sendo atualizado
+        if (isset($_POST['atualizar_cliente'])) {
+            // Obter os dados do formulário
+            $clienteID = $_POST['cliente_id'];
+            $nomeCliente = $_POST['nome_cliente'];
+            $enderecoCliente = $_POST['endereco_cliente'];
+            $telefoneCliente = $_POST['telefone_cliente'];
+            $emailCliente = $_POST['email_cliente'];
+
+            // Preparar a consulta SQL para atualizar os dados
+            $sql = "UPDATE cliente SET nome_cliente='$nomeCliente', endereco_cliente='$enderecoCliente', telefone_cliente='$telefoneCliente', email_cliente='$emailCliente' WHERE cliente_id=$clienteID";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Cliente alterado com sucesso.";
+            } else {
+                echo "Erro ao alterar o registro do cliente: " . $conn->error;
+            }
+        }
     }
 
-    // Fechar a conexão
+    // Fechar a conexão com o banco de dados
     $conn->close();
     ?>
+
+    <h1>Meu Site</h1>
+
+    <!-- Formulário para adicionar cliente -->
+    <h2>Adicionar Cliente</h2>
+    <form action="index.php" method="post">
+        <label for="nome_cliente">Nome:</label>
+        <input type="text" name="nome_cliente" id="nome_cliente" required><br>
+
+        <label for="endereco_cliente">Endereço:</label>
+        <input type="text" name="endereco_cliente" id="endereco_cliente" required><br>
+
+        <label for="telefone_cliente">Telefone:</label>
+        <input type="text" name="telefone_cliente" id="telefone_cliente" required><br>
+
+        <label for="email_cliente">Email:</label>
+        <input type="email" name="email_cliente" id="email_cliente" required><br>
+        
+        <input type="submit" name="adicionar_cliente" value="Adicionar Cliente" class="touchable-button">
+    </form>
+    <a href="processar_cliente.php" class="touchable-button">Editar Clientes</a>
+    <!-- Coloque o código específico para exibir a lista de clientes aqui -->
 
 </body>
 </html>
---
+++ CRUD/processar_cliente.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processar Cliente</title>
+    <link rel="stylesheet" href="processar_cliente.css">
+</head>
+
+<body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "scriptbdseguro";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+}
+
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Verifica se o botão de alterar foi clicado
+    if (isset($_POST["alterar"])) {
+        $cliente_id = $_POST["alterar"];
+        // Implemente a lógica para ação de alterar aqui
+        $novo_nome = $POST["novo_nome$cliente_id"];
+        $novo_endereco = $POST["novo_endereco$cliente_id"];
+        $novo_telefone = $POST["novo_telefone$cliente_id"];
+        $novo_email = $POST["novo_email$cliente_id"];
+
+        $sql = "UPDATE cliente SET nome_cliente='$novo_nome', endereco_cliente='$novo_endereco', telefone_cliente='$novo_telefone', email_cliente='$novo_email' WHERE cliente_id=$cliente_id";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Cliente alterado com sucesso!";
+        } else {
+            echo "Erro ao alterar o cliente: " . $conn->error;
+        }
+    }
+
+    // Verifica se o botão de excluir foi clicado
+    elseif (isset($_POST["excluir"])) {
+        $cliente_id = $_POST["excluir"];
+        // Implemente a lógica para ação de excluir aqui
+        $sql = "DELETE FROM cliente WHERE cliente_id=$cliente_id";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "Cliente excluído com sucesso!";
+        } else {
+            echo "Erro ao excluir o cliente: " . $conn->error;
+        }
+    }
+}
+
+// Recuperar e exibir a lista de clientes
+$sqlClientes = "SELECT cliente_id, nome_cliente, endereco_cliente, telefone_cliente, email_cliente FROM cliente";
+$resultClientes = $conn->query($sqlClientes);
+
+if ($resultClientes->num_rows > 0) {
+    echo "<br><a href='index.php'><button>Voltar para a Página Inicial</button></a>";
+    echo "<h2>Lista de Clientes:</h2>";
+    echo "<form method='post' action='processar_cliente.php'>";
+    echo "<table border='1'>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Endereço</th>
+                <th>Telefone</th>
+                <th>Email</th>
+                <th>Ação</th>
+            </tr>";
+
+    while ($row = $resultClientes->fetch_assoc()) {
+        echo "<tr>
+                <td>" . $row["cliente_id"] . "</td>
+                <td><input type='text' name='novo_nome_" . $row["cliente_id"] . "' value='" . $row["nome_cliente"] . "'></td>
+                <td><input type='text' name='novo_endereco_" . $row["cliente_id"] . "' value='" . $row["endereco_cliente"] . "'></td>
+                <td><input type='text' name='novo_telefone_" . $row["cliente_id"] . "' value='" . $row["telefone_cliente"] . "'></td>
+                <td><input type='text' name='novo_email_" . $row["cliente_id"] . "' value='" . $row["email_cliente"] . "'></td>
+                <td>
+                    <button type='submit' name='alterar' value='" . $row["cliente_id"] . "'>Alterar</button>
+                    <button type='submit' name='excluir' value='" . $row["cliente_id"] . "'>Excluir</button>
+                </td>
+            </tr>";
+    }
+
+    echo "</table>";
+    echo "</form>";
+
+    
+} else {
+    echo "<br><a href='index.php'><button>Voltar para a Página Inicial</button></a>";
+    echo "Nenhum cliente encontrado.";
+}
+
+// Fecha a conexão com o banco de dados
+$conn->close();
+?>
+</body>
+
+</html>
